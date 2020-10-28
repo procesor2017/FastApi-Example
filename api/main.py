201b742f4cc8145d0dd1c3f30af513ccf6bb5c43
janegermaier <jan.egermaier@tesena.com>
2020-10-28 14:35:20 +0100

    new try

--- api/main.py
+++ api/main.py
@@ -1,5 +1,3 @@
-import os
-import shutil
 from pathlib import Path
 from os import listdir
 from os.path import isfile, join
@@ -7,12 +5,12 @@ from pydantic import BaseModel
 
 from typing import Optional
 from fastapi import FastAPI, File, UploadFile
-from api.src.files import convert_to_string, return_string, upload_new_file
-from api.src.joke import read_json, get_joke, insert_joke, delete_joke_by
-
+from src.files import convert_to_string, return_string, upload_new_file
+from src.joke import read_json, get_joke, insert_joke, delete_joke_by
 
 app = FastAPI()
-cwd_path = Path('./save_folder')
+cwd_path = Path('../save_folder')
+
 
 class Joke(BaseModel):
     text: str
@@ -28,38 +26,46 @@ class Joke(BaseModel):
 def read_root():
     return {"Hello": "World"}
 
+
 @app.post('/files/check_content_json')
 async def upload_file(file: UploadFile = File(...)):
     content = await file.read()
     json_string = convert_to_string(content)
     return {'file_contents': json_string}
-    
+
+
 @app.post("/files/upload")
 def create_file(file: UploadFile = File(...)):
     return upload_new_file(file)
+
 
 @app.get('/files/savefolder')
 def check_save_folder():
     onlyfiles = [f for f in listdir(cwd_path) if isfile(join(cwd_path, f))]
     return onlyfiles
 
+
 @app.get('/files/savefolder/')
-def read_file_from_save(id: int = 0):
-    data = return_string(id)
+def read_file_from_save(file_id: int = 0):
+    data = return_string(file_id)
     return data
+
 
 @app.get('/jokes/alljokes')
 def get_all_jokes():
     return read_json()
 
+
 @app.get('/jokes/')
-def return_specific_joke(id: int = 0):
-    return get_joke(id)
+def return_specific_joke(joke_id: int = 0):
+    return get_joke(joke_id)
+
 
 @app.post('/jokes/insert/')
 def input_joke(joke: Joke):
     return insert_joke(joke)
 
+
 @app.post('/jokes/delete/')
-def delete_joke(id: int):
-    return delete_joke_by(id)
+def delete_joke(joke_id: int):
+    return delete_joke_by(joke_id)
