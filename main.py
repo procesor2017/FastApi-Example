9b1fd8bb0d358f856e777678df2d7ada1ba1cb91
janegermaier <jan.egermaier@tesena.com>
2020-10-28 14:49:06 +0100

    final code

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from pydantic import BaseModel
 
 from typing import Optional
 from fastapi import FastAPI, File, UploadFile
-from src.files import convert_to_string, return_string, upload_new_file
-from src.joke import read_json, get_joke, insert_joke, delete_joke_by
+from api.src.files import convert_to_string, return_string, upload_new_file
+from api.src.joke import read_json, get_joke, insert_joke, delete_joke_by
+
 
 app = FastAPI()
-cwd_path = Path('../save_folder')
+cwd_path = Path('save_folder')
 
 
 class Joke(BaseModel):
